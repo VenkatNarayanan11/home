e06d931b57493a3831a8c9f76e56d7263bb97a8b
Venkat Narayanan Balachandran <venkatnarayanan107@gmail.com>
2021-04-10 23:31:11 -0400

    Updates

---
+++ precache-manifest.64496c291e9d7c57cd30b08ec04d3b4f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1142a9ba7885462e6c08c7e20a4a8d2b",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "062bb2e04c428956ee91",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/home/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "062bb2e04c428956ee91",
+    "url": "/home/static/js/main.eb8de575.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "a71e117a9cf7ffc97838600af6e2f947",
+    "url": "/home/static/media/Venkat.a71e117a.jpg"
+  },
+  {
+    "revision": "c6ab48cdc48bdf4f13e7fc7de3634363",
+    "url": "/home/static/media/resume.c6ab48cd.pdf"
+  }
+]);
